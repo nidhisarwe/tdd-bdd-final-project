588a6df80cb56a7ae79754259d1cef300c9a976a
Nidhi Sarwe <166297185+nidhisarwe@users.noreply.github.com>
2024-10-29 13:25:13 +0530

    Added LIST ALL testcases test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -39,3 +39,16 @@ def test_delete_a_product(self):
         product.delete()
         self.assertEqual(len(Product.all()), 0)
 
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        # Create 5 Products
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # See if we get back 5 products
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+
