dbcd1d4affa5bd35f820e7c60ef8d438c628d1cf
Nidhi Sarwe <166297185+nidhisarwe@users.noreply.github.com>
2024-10-29 13:16:51 +0530

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,34 +1,27 @@
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# pylint: disable=too-few-public-methods
-
 """
-Test Factory to make fake objects for testing
+AccountFactory class using FactoryBoy
+
+Documentation on Faker Providers:
+    https://faker.readthedocs.io/en/master/providers/baseprovider.html
+
+Documentation on Fuzzy Attributes:
+    https://factoryboy.readthedocs.io/en/stable/fuzzy.html
+
 """
 import factory
-from factory.fuzzy import FuzzyChoice, FuzzyDecimal
-from service.models import Product, Category
+from datetime import date
+from factory.fuzzy import FuzzyChoice, FuzzyDate
+from models.account import Account
 
-
-class ProductFactory(factory.Factory):
-    """Creates fake products for testing"""
+class AccountFactory(factory.Factory):
+    """ Creates fake Accounts """
 
     class Meta:
-        """Maps factory to data model"""
-
-        model = Product
+        model = Account
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    phone_number = factory.Faker("phone_number")
+    disabled = FuzzyChoice(choices=[True, False])
+    date_joined = FuzzyDate(date(2008, 1, 1))
